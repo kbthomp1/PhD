2e938cf0530c8f9f645c45b3433f5694cfba6619
kbthomps <kyle.b.thompson@nasa.gov>
2016-08-17 08:59:59 -0400

    Save changes to ruby file and fixed Chapter-1 makefile

--- thesis/add_chapter.rb
+++ thesis/add_chapter.rb
@@ -19,13 +19,22 @@ def move_chapter(chapter)
   new_chapter_dir = "Chapter-#{chapter+1}"
   old_tex = "#{old_chapter_dir}/Chapter-#{chapter}.tex"
   new_tex = "#{old_chapter_dir}/Chapter-#{chapter+1}.tex"
+
   clean_cmd = "(cd #{old_chapter_dir} && make clean)"
   system clean_cmd
   puts "moving #{old_tex} to #{new_tex}"
   puts "moving #{old_chapter_dir} to #{new_chapter_dir}"
 end
 
+def make_new_chapter(new_chapter)
+  new_dir = "Chapter-#{new_chapter}"
+  FileUtils.mkdir(new_dir)
+  FileUtils.ln_s("chapter-makefile","#{new_dir}/Makefile")
+  FileUtils.touch("#{new_dir}/Chapter-#{new_chapter}.tex")
+end
+
 new_chapter = ARGV[0]
+new_chapter = new_chapter.to_i
 
 if (new_chapter == nil)
   puts "usage: ruby add_chapter <new chapter number>"
@@ -34,8 +43,10 @@ end
 
 max_chapter = find_max_chapter()
 chapter = max_chapter
-while chapter >= new_chapter.to_i
+while chapter >= new_chapter
   move_chapter(chapter)
   chapter -= 1
 end
+
+make_new_chapter(new_chapter)
 
